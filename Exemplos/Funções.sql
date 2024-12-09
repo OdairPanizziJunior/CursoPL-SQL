11edc7d76452f8ab8b8b92f9d9ab779aa9538e6f
Odair Panizzi Júnior <101676143+OdairPanizziJunior@users.noreply.github.com>
2024-12-09 14:06:59 -0300

    Update

--- Exemplos/Funções.sql
+++ Exemplos/Funções.sql
@@ -13,15 +13,20 @@
 
 -- Explicação:
 
---      CREATE OR REPLACE FUNCTION      =>    Cria a função, substituindo-a se já existir.
---      RETURN VARCHAR2                 =>    Declara que o valor de retorno da função será do tipo VARCHAR2.
---      IS                              =>    Inicia a definição do corpo da função.
---      BEGIN...END;                    =>    Define o bloco executável da função.
---      RETURN 'HELLO WORLD!';          =>    Retorna o valor desejado.
+--      CREATE OR REPLACE FUNCTION    =>  Cria a função, substituindo-a se já existir.
+--      RETURN VARCHAR2               =>  Declara que o valor de retorno da função será do tipo VARCHAR2.
+--      IS                            =>  Inicia a definição do corpo da função.
+--      BEGIN...END;                  =>  Define o bloco executável da função.
+--      RETURN 'HELLO WORLD!';        =>  Retorna o valor desejado.
 
 
---/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/
 
+
+
+
+
+
+-- Como criar uma função com parâmetros:
 
 CREATE OR REPLACE FUNCTION SAUDACAO_PESSOA (
 	NOME IN VARCHAR2, 
@@ -32,7 +37,16 @@ BEGIN
 	RETURN 'OLÁ ' || NOME || ', SUA IDADE É ' || IDADE || ' ANOS.';
 END;
 
+-- Chamando a função:
 
+    SELECT SAUDACAO_PESSOA('ODAIR', 34) 
+    FROM DUAL
 
-SELECT SAUDACAO_PESSOA('ODAIR', 34) FROM DUAL
+-- Explicação:
+
+--      CREATE OR REPLACE FUNCTION                              =>  Cria a função, substituindo-a se já existir.
+--      RETURN VARCHAR2                                         =>  Declara que o valor de retorno da função será do tipo VARCHAR2 (não precisa retornar os dois).
+--      IS                                                      =>  Inicia a definição do corpo da função.
+--      BEGIN...END;                                            =>  Define o bloco executável da função.
+--      RETURN 'OLÁ [NOME], SUA IDADE É [IDADE] ANOS. WORLD!';  =>  Retorna o valor desejado, só que agora com os parâmetros.
 
